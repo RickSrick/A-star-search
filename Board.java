69286379a7831605458fba567d47e8c28df20aec
RickSrick <79211132+RickSrick@users.noreply.github.com>
2021-12-03 12:12:14 +0100

    System Correction

--- Board.java
+++ Board.java
@@ -9,7 +9,7 @@ public final class Board implements Comparable<Board> {
     final boolean[] lastmove = new boolean[4]; //0 up ,  1 down , 2 left, 3 right 
     final int [] zero = new int[2];
 
-    public Board(int[][] tiles) { this(tiles,null, 0, -1 ); }
+    public Board(int[][] tiles) { this(tiles,null, 0, -1); }
     
     public Board(int[][] tiles, Board p, int m, int lm) {
         board = tiles;
@@ -23,7 +23,6 @@ public final class Board implements Comparable<Board> {
 
     public int getPriority() { return data[1] + data [2]; }
 
-    //O(4n^2)
     public Stack<Board> getChildren() {
         final Stack<Board> children = new Stack<Board>(); //max child num
         if((!lastmove[0]) ) {
@@ -56,7 +55,6 @@ public final class Board implements Comparable<Board> {
     public Board Vertical (int index) {
         int pos = zero[0] + index;
         if(pos < 0 || pos >= data[0]) return null;
-
         int[][] tmp = copymatrix(board);
 
         tmp[zero[0]][zero[1]] ^= tmp[pos][zero[1]];
@@ -73,13 +71,12 @@ public final class Board implements Comparable<Board> {
     public Board Horizontal (int index) {
         int pos = zero[1] + index;
         if(pos < 0 || pos >= data[0]) return null;
-
         int[][] tmp = copymatrix(board);
-
+        
         tmp[zero[0]][zero[1]] ^= tmp[zero[0]][pos];
         tmp[zero[0]][pos] ^= tmp[zero[0]][zero[1]];
         tmp[zero[0]][zero[1]] ^= tmp[zero[0]][pos];
-
+    
         byte m = 0;
         if(index > 0) { m = 2; }
         else {m = 3;}
@@ -87,20 +84,19 @@ public final class Board implements Comparable<Board> {
         return new Board(tmp, this, data[1]+1, m);
     }
 
-    public String toString() {
-        return Arrays.deepToString(board);
-    }
+    public String toString() { return Arrays.deepToString(board); }
 
 
     public int manhattan() {
         int tmp = 0;
-
+        
         for (byte i = 0; i < data[0]; i++) {
             for (byte j = 0; j < data[0]; j++) {
                 if (board[i][j] == 0) { zero[0] = i; zero[1] = j; }
                 tmp += (board[i][j] == 0) ? 0 : Math.abs(i - (board[i][j]-1)/data[0]) + Math.abs(j - (board[i][j]-1)%data[0]);
             }
         }
+
         return tmp;
     }
 
@@ -113,7 +109,6 @@ public final class Board implements Comparable<Board> {
     public boolean equals(Object obj) {
         
         if(obj == this) return true;
-        //return Arrays.deepEquals(this.board, ((Board) obj).board); 
         return ((Board) obj).toString().equals(this.toString()); 
     }
 }
--- Solver.java
+++ Solver.java
@@ -4,68 +4,69 @@ import java.util.Stack;
 
 public class Solver {
 
-    static  PriorityQueue<Board> pq = new PriorityQueue<Board>();
-    static  HashSet<Board> hm = new HashSet<Board>();
+    static final PriorityQueue<Board> pq = new PriorityQueue<Board>();
+    static final HashSet<Board> hm = new HashSet<Board>();
 
     public static void main(String[] args) {
+
+        double tot = 0;
+        int maxTest = 1;
+
+        for (int i = 0; i < maxTest; i++){
 
         long x = System.currentTimeMillis();
 
 
         int[][] intial = {
-                            {5, 1, 2},
-                            {4, 8, 3},
-                            {0, 6, 7}
-                         };
+                            {1, 5, 6},
+                            {7, 4, 3},
+                            {0, 2, 8}
+                        };
         final Board b = new Board(intial);
         
         pq.add(b);
+        hm.add(b);
         while(pq.peek().data[2] != 0){            
-            solve(pq.peek());
+            solve(pq.poll());
         }
 
 
         final Stack<Board> res = printpath(pq.peek());
 
-        System.out.println(res.size());
+        System.out.println(res.size()-1);
         while(res.size() != 0){
             System.out.println(res.pop());
         }
 
         long y =System.currentTimeMillis();
 
-        System.out.println(y-x);
+            tot += (y-x);
+        }
+
+        System.out.println(tot/maxTest);
     }
 
     private static Stack<Board> printpath(Board start) {
-       final Stack<Board> ll = new Stack<Board>();
-       Board curr = start;
-       while(curr.pater != null) {
-
-            ll.add(curr);
-            curr = curr.pater;
-       }
-
-       ll.add(curr);
-       return ll;
-        
+        final Stack<Board> ll = new Stack<Board>();
+        while(start.pater != null) {
+            ll.add(start);
+            start = start.pater;
+        }
+        ll.add(start);
+        return ll;
     }
 
     static void solve (Board b){
         final Stack<Board> children = b.getChildren();
-        pq.remove(b);
-        hm.add(b);
         while(children.size() != 0) {
             
             Board bson = children.pop();
-
-            if(bson != null){
+            if(bson != null && !hm.contains(bson)) {
                 pq.add(bson);
-                if(!hm.contains(bson)) { hm.add(bson); }
+                hm.add(bson); 
             }
+            
         }
     }
-
-
     
 }
