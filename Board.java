93268cbe3eb313bd4aa7aff50e59fc83f8411c1c
RickSrick <79211132+RickSrick@users.noreply.github.com>
2021-12-11 13:36:01 +0100

    Indentation Fixed

--- Board.java
+++ Board.java
@@ -1,123 +1,122 @@
 public final class Board implements Comparable<Board> {
 
-    Board pater;
-    int [][] board;
-    static int size;
-    int [] data = new int[2]; //0 move , 1 manhattan
-    final boolean[] lastmove = new boolean[4]; //0 up ,  1 down , 2 left, 3 right 
-    final int [] zero = new int[2];
+	Board pater;
+	int [][] board;
+	static int size;
+	int[] data = new int[2]; //0 move , 1 manhattan
+	final boolean[] lastmove = new boolean[4]; //0 up ,  1 down , 2 left, 3 right 
+	final int [] zero = new int[2];
 
-    public Board(int[][] tiles) { 
-        board = tiles;
-        pater = null;
-        size = tiles.length;
-        data[0] = 0;
-        data[1] = manhattan();        
-    }
-    
-    public Board(int[][] tiles, Board p, int m, int lm, int mh, int zerox, int zeroy) {
-        board = tiles;
-        pater = p;
-        data[0] = m;
-        data[1] = mh; 
-        zero[0] = zerox;
-        zero[1] = zeroy;
-        lastmove[lm] = true;
-    }
+	public Board(int[][] tiles) {
+		board = tiles;
+		pater = null;
+		size = tiles.length;
+		data[0] = 0;
+		data[1] = manhattan();
+	}
+	public Board(int[][] tiles, Board p, int m, int lm, int mh, int zerox, int zeroy) {
+		board = tiles;
+		pater = p;
+		data[0] = m;
+		data[1] = mh;
+		zero[0] = zerox;
+		zero[1] = zeroy;
+		lastmove[lm] = true;
+	}
 
-    public int getPriority() { return data[0] + data[1]; }
+	public int getPriority() { return data[0] + data[1]; }
 
-    public Board[] getChildren() {
-        final Board[] children = new Board[4];
-        int k = 0;
-        if(!lastmove[0]) {
-            children[k++] = Vertical(-1);
-        } 
-        if(!lastmove[1]) {
-            children[k++] = Vertical(1);
-        }
-        if(!lastmove[2]) {
-            children[k++] = Horizontal(-1);
-        }
-        if(!lastmove[3]) {
-            children[k++] = Horizontal(1);
-        }
-        return children;
-    }
+	public Board[] getChildren() {
+		final Board[] children = new Board[4];
+		int k = 0;
+		if(!lastmove[0]) {
+			children[k++] = Vertical(-1);
+		}
+		if(!lastmove[1]) {
+			children[k++] = Vertical(1);
+		}
+		if(!lastmove[2]) {
+			children[k++] = Horizontal(-1);
+		}
+		if(!lastmove[3]) {
+			children[k++] = Horizontal(1);
+		}
+		return children;
+	}
 
-    private static int[][] copymatrix (int[][] pater) {
-        final int[][] tmp = new  int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                tmp[i][j] = pater[i][j];
-            }
-        }
-        return tmp; 
-    }
+	private static int[][] copymatrix (int[][] pater) {
+		final int[][] tmp = new  int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				tmp[i][j] = pater[i][j];
+			}
+		}
+		return tmp; 
+	}
 
-    public Board Vertical (int index) {
-        int pos = zero[0] + index;
-        if(pos < 0 || pos >= size) return null;
-        int[][] tmp = copymatrix(board);
-        int mh = data[1];
+	public Board Vertical (int index) {
+		int pos = zero[0] + index;
+		if(pos < 0 || pos >= size) return null;
+		int[][] tmp = copymatrix(board);
+		int mh = data[1];
 
-        mh -=  Math.abs(pos - (tmp[pos][zero[1]]-1)/size) + Math.abs(zero[1] - (tmp[pos][zero[1]]-1)%size);
+		mh -=  Math.abs(pos - (tmp[pos][zero[1]]-1)/size) + Math.abs(zero[1] - (tmp[pos][zero[1]]-1)%size);
 
-        tmp[zero[0]][zero[1]] ^= tmp[pos][zero[1]];
-        tmp[pos][zero[1]] ^= tmp[zero[0]][zero[1]];
-        tmp[zero[0]][zero[1]] ^= tmp[pos][zero[1]];
+		tmp[zero[0]][zero[1]] ^= tmp[pos][zero[1]];
+		tmp[pos][zero[1]] ^= tmp[zero[0]][zero[1]];
+		tmp[zero[0]][zero[1]] ^= tmp[pos][zero[1]];
 
-        mh += Math.abs(zero[0] - (tmp[zero[0]][zero[1]]-1)/size) + Math.abs(zero[1] - (tmp[zero[0]][zero[1]]-1)%size);
+		mh += Math.abs(zero[0] - (tmp[zero[0]][zero[1]]-1)/size) + Math.abs(zero[1] - (tmp[zero[0]][zero[1]]-1)%size);
 
-        byte lastMove = (index > 0) ? (byte)0 : 1;
-        return new Board(tmp, this, data[0]+1, lastMove, mh, pos, zero[1]);
-    }
+		byte lastMove = (index > 0) ? (byte)0 : 1;
+		return new Board(tmp, this, data[0]+1, lastMove, mh, pos, zero[1]);
+	}
 
-    public Board Horizontal (int index) {
-        int pos = zero[1] + index;
-        if(pos < 0 || pos >= size) return null;
-        int[][] tmp = copymatrix(board);
-        int mh = data[1];
+	public Board Horizontal (int index) {
+		int pos = zero[1] + index;
+		if(pos < 0 || pos >= size) return null;
+		int[][] tmp = copymatrix(board);
+		int mh = data[1];
 
-        mh -=  Math.abs(zero[0] - (tmp[zero[0]][pos]-1)/size) + Math.abs(pos - (tmp[zero[0]][pos]-1)%size);
-        
-        tmp[zero[0]][zero[1]] ^= tmp[zero[0]][pos];
-        tmp[zero[0]][pos] ^= tmp[zero[0]][zero[1]];
-        tmp[zero[0]][zero[1]] ^= tmp[zero[0]][pos];
+		mh -=  Math.abs(zero[0] - (tmp[zero[0]][pos]-1)/size) + Math.abs(pos - (tmp[zero[0]][pos]-1)%size);
+		
+		tmp[zero[0]][zero[1]] ^= tmp[zero[0]][pos];
+		tmp[zero[0]][pos] ^= tmp[zero[0]][zero[1]];
+		tmp[zero[0]][zero[1]] ^= tmp[zero[0]][pos];
 
-        mh += Math.abs(zero[0] - (tmp[zero[0]][zero[1]]-1)/size) + Math.abs(zero[1] - (tmp[zero[0]][zero[1]]-1)%size);
+		mh += Math.abs(zero[0] - (tmp[zero[0]][zero[1]]-1)/size) + Math.abs(zero[1] - (tmp[zero[0]][zero[1]]-1)%size);
 
-        byte lastMove = (index > 0) ? (byte)2 : 3;
-        return new Board(tmp, this, data[0]+1, lastMove, mh, zero[0], pos);
-    }
-    
-    public int manhattan() {
-        int tmp = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (board[i][j] == 0) { zero[0] = i; zero[1] = j; }
-                tmp += (board[i][j] == 0) ? 0 : Math.abs(i - (board[i][j]-1)/size) + Math.abs(j - (board[i][j]-1)%size);
-            }
-        }
-        return tmp;
-    }
+		byte lastMove = (index > 0) ? (byte)2 : 3;
+		return new Board(tmp, this, data[0]+1, lastMove, mh, zero[0], pos);
+	}
+	
+	public int manhattan() {
+		int tmp = 0;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (board[i][j] == 0) { zero[0] = i; zero[1] = j; }
+				tmp += (board[i][j] == 0) ? 0 : Math.abs(i - (board[i][j]-1)/size) + Math.abs(j - (board[i][j]-1)%size);
+			}
+		}
+		return tmp;
+	}
 
-    @Override
-    public String toString() { 
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                sb.append(board[i][j]);
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-    }
+	@Override
+	public String toString() { 
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				sb.append(board[i][j]);
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
+	}
 
-    @Override
-    public int compareTo(Board b) { return this.getPriority() - b.getPriority(); }
+	@Override
+	public int compareTo(Board b) { return this.getPriority() - b.getPriority(); }
 
-    @Override
-    public boolean equals(Object obj) { return ((Board) obj).toString().equals(this.toString()); }
+	@Override
+	public boolean equals(Object obj) { return ((Board) obj).toString().equals(this.toString()); }
 }
--- Solver.java
+++ Solver.java
@@ -3,66 +3,65 @@ import java.util.PriorityQueue;
 
 public class Solver {
 
-    static final PriorityQueue<Board> pq = new PriorityQueue<Board>();
-    static final HashSet<Board> hm = new HashSet<Board>();
+	static final PriorityQueue<Board> pq = new PriorityQueue<Board>();
+	static final HashSet<Board> hm = new HashSet<Board>();
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        double tot = 0;
-        int maxTest = 1000;
+		double tot = 0;
+		int maxTest = 1000;
 
-        for (int i = 0; i < maxTest; i++){
+		for (int i = 0; i < maxTest; i++){
 
-        long x = System.currentTimeMillis();
+		long x = System.currentTimeMillis();
 
 
-        int[][] intial = {
-            {1, 5, 6},
-            {7, 4, 3},
-            {0, 2, 8}
-        };
-        final Board b = new Board(intial);
-        
-        pq.add(b);
-        hm.add(b);
-        while(pq.peek().data[1] != 0){ solve(pq.poll()); }
+		int[][] intial = {
+			{1, 5, 6},
+			{7, 4, 3},
+			{0, 2, 8}
+		};
+		final Board b = new Board(intial);
+		
+		pq.add(b);
+		hm.add(b);
+		while(pq.peek().data[1] != 0){ solve(pq.poll()); }
 
-        final Board[] result = printPath(pq.peek());
+		final Board[] result = printPath(pq.peek());
 
-        System.out.println(result.length-1);
-        for (int j = 0; j < result.length; j++) {
-            System.out.println(result[j]);
-        }
+		System.out.println(result.length-1);
+		for (int j = 0; j < result.length; j++) {
+			System.out.println(result[j]);
+		}
 
-        long y =System.currentTimeMillis();
+		long y =System.currentTimeMillis();
 
-            tot += (y-x);
-        }
+			tot += (y-x);
+		}
 
-        System.out.println(tot/maxTest);
-    }
+		System.out.println(tot/maxTest);
+	}
 
-    static Board[] printPath(Board start){
-        final int length = start.data[0];
-        final Board[] path = new Board[length+1];
+	static Board[] printPath(Board start){
+		final int length = start.data[0];
+		final Board[] path = new Board[length+1];
 
-        for (int i = length; i > 0; i--) {
-            path[i] = start;
-            start = start.pater;
-        }
+		for (int i = length; i > 0; i--) {
+			path[i] = start;
+			start = start.pater;
+		}
 
-        path[0] = start;
-        return path;
-    }
+		path[0] = start;
+		return path;
+	}
 
-    static void solve (Board b){
-        final Board[] children = b.getChildren();
-        for(int i = 0; i < 4; i++) {
-            if(children[i] != null && !hm.contains(children[i])) {
-                pq.add(children[i]);
-                hm.add(children[i]);
-            }            
-        }
-    }
-    
+	static void solve (Board b){
+		final Board[] children = b.getChildren();
+		for(int i = 0; i < 4; i++) {
+			if(children[i] != null && !hm.contains(children[i])) {
+				pq.add(children[i]);
+				hm.add(children[i]);
+			}
+		}
+	}
 }
